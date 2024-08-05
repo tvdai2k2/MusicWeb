aa3c6cfe5be20413aab8b7bae011580f3272063b
tvdai2k2 <tvdai2k2@gmail.com>
2024-08-05 22:52:09 +0700

    Adđ js

---
+++ Scripts/model-dai.js
@@ -0,0 +1,12 @@
+﻿$(document).ready(function () {
+    $('.info-post .col-md-2').hover(
+        function () {
+            $(this).addClass(["dark-light-bg", "rounded"]);
+            $(this).find('.play-btn').css("visibility","visible")
+        },
+        function () {
+            $(this).removeClass(["dark-light-bg", "rounded"])
+            $(this).find('.play-btn').css("visibility", "hidden")
+        }
+    )
+});
